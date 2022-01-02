c729588661c7f5b454e583b4b04b19211a6bea73
Dogmin <kdm3578@gmail.com>
2022-01-02 15:27:39 +0900

    Start create-react-qpp(#5.0~#5.1)

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import propTypes from "prop-types";
+import styles from "./Button.module.css";
+function Button({ text }) {
+  return <button className={styles.btn}>{text}</button>;
+}
+export default Button;
+
+Button.protoTypes = {
+  text: propTypes.string.isRequired,
+};
